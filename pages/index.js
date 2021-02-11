5a6464ea9ef102f80dd1b44528fffb807507a306
oskvr <oskar.puustinen@gmail.com>
2021-02-11 03:25:29 +0100

    Init but still pretty finished

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,100 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [tabTrigger, setTabTrigger] = useState("");
+  const [description, setDescription] = useState("");
+  const [body, setBody] = useState("");
+  const [generatedCode, setGeneratedCode] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+  const [tabIndex, setTabIndex] = useState(1);
+  const bodyArray = body.split("\n").map((line) => `"${line}", \n`);
+  useEffect(() => {
+    setGeneratedCode(
+      `"${description}": {
+        "prefix": "${tabTrigger}",
+        "body": [  
+          ${bodyArray
+            .map((line, index) => {
+              if (index === 0) {
+                return line;
+              } else {
+                return " ".repeat(10) + line;
+              }
+            })
+            .join("")}
+        ],
+        "description": "${description}"
+      }`
+    );
+    setIsCopied(false);
+    console.log(isCopied);
+  }, [tabTrigger, description, body]);
+  function copyToClipboard() {
+    navigator.clipboard.writeText(generatedCode);
+    setIsCopied(true);
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="container mx-auto ">
       <Head>
-        <title>Create Next App</title>
+        <title>Snippet Generator App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex flex-wrap gap-5">
+        <section>
+          <div className="bg-gray-500 p-2 space-x-5">
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="border p-4"
+              placeholder="Description"
+            />
+            <input
+              type="text"
+              value={tabTrigger}
+              onChange={(e) => setTabTrigger(e.target.value)}
+              className="border p-4"
+              placeholder="Tab trigger"
+            />
+          </div>
+          <textarea
+            className="flex-1 border-2 bg-blue-50 p-2 h-4/6"
+            value={body}
+            rows="50"
+            cols="70"
+            onChange={(e) => setBody(e.target.value)}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+        </section>
+        <section>
+          <header className="p-5 bg-gray-500">
+            <h3 className="text-white text-2xl text-center">Output:</h3>
+          </header>
+          <div className="relative group">
+            <div className="absolute top-0 left-0 w-full h-full bg-green-200 bg-opacity-0 group-hover:bg-opacity-20 grid place-items-center transition">
+              <button
+                onClick={copyToClipboard}
+                className="relative bg-blue-400 w-32 hover:bg-blue-500 text-white p-3 opacity-0 group-hover:opacity-100 transition"
+              >
+                {isCopied ? "Copied" : "Copy snippet"}
+              </button>
+            </div>
+            <div className="bg-blue-50 border p-2">
+              <pre>{generatedCode}</pre>
+            </div>
+          </div>
+        </section>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
+  function handleKeyDown(e) {
+    if (e.ctrlKey && e.key === "i") {
+      const selectionStart = e.target.selectionStart;
+      const selectionEnd = e.target.selectionEnd;
+      const firstHalf = body.substr(0, selectionStart);
+      const secondHalf = body.substr(selectionEnd, body.length);
+      setBody(firstHalf + `\${1:example}` + secondHalf);
+    }
+  }
 }
